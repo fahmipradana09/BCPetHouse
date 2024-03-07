1c4000977a97a1548ce59fe14fdcab55dd200a71
fahmi pradana <74582392+fahmipradana09@users.noreply.github.com>
2024-03-07 11:26:14 +0700

    Update view in rawat inap

--- app/Views/admin/rawat_inap/detail.php
+++ app/Views/admin/rawat_inap/detail.php
@@ -7,116 +7,96 @@
         <div class="card-header pb-0">
           <h4>Rawat Inap</h4>
         </div>
-        <form class="row m-3" action="<?= base_url(); ?>RawatInap/save/<?= $dataInap['id']?>" method="post">
+        <form class="container-fluid mt-5 mx-3" action="<?= base_url(); ?>RawatInap/save/<?= $dataInap['id']?>" method="post">
           <?= csrf_field(); ?>
-          <div class="col-md-5 mt-3">
-            <label for="inputPetName" class="form-label text-truncate">Pet Name</label>
-            <div>
-            <input type="text" name="petName" class="form-control <?=($petName) ? 'is-invalid' : ''; ?>" id="inputPetName" value="<?= $dataInap['name'] ? $dataInap['name'] : '' ?>" autofocus>
-            <div class="invalid-feedback">
-                <?= $petName ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-5 mt-3">
-            <label for="inputOwnerName" class="form-label">Owner Name</label>
-            <div>
-                <input type="text" name="ownerName" class="form-control <?=($ownerName) ? 'is-invalid' : ''; ?>" id="inputOwnerName" placeholder="Type Here...."  value="<?= $dataInap['owner_name'] ? $dataInap['owner_name'] : '' ?>">
-                <div class="invalid-feedback">
-                <?= $ownerName ?>
-              </div>
-            </div>
-          </div>
-          
-          <div class="col-md-2 mt-3">
-          <label for="inputAge" class="form-label text-truncate">Age</label>
-            <div>
-                <input type="text" name="age" class="form-control <?=($age) ? 'is-invalid' : ''; ?>" id="inputAge" placeholder="Type Here...." value="<?= $dataInap['age'] ? $dataInap['age'] : '' ?>">
-                <div class="invalid-feedback">
-                  <?= $age ?>
+          <div class="row col-12">
+          <div class="column col-6">
+              <div class="row align-items-center">
+                <label for="petName" class="col-auto col-form-label">Pet Name :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="petName" value=" <?= $dataInap['name'] ? $dataInap['name'] : '' ?>">
+                  </div>
                 </div>
-             </div>
-          </div>
+              </div>
+            
+              <div class="row align-items-center">
+                <label for="age" class="col-auto col-form-label ">Age :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="age" value="<?= $dataInap['age'] ? $dataInap['age'] : '' ?>">
+                  </div>
+                </div>
+              </div>
 
-          <div class="col-md-12 mt-3">
-            <label for="inputAddress" class="form-label text-truncate">Address</label>
-            <div>
-              <textarea class="form-control <?=($address) ? 'is-invalid' : ''; ?>" name="address" id="inputAddress" rows="3"><?= $dataInap['address'] ? $dataInap['address'] : '' ?></textarea>
-              <div class="invalid-feedback">
-                <?= $address ?>
+              <div class="row align-items-center">
+                <label for="phone" class="col-auto col-form-label">Phone Number :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="phone" value="<?= $dataInap['phone'] ? $dataInap['phone'] : '' ?>">
+                  </div>
+                </div>
+              </div>
+
+              <div class="row align-items-center">
+                <label for="animalType" class="col-auto col-form-label">Animal Type :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="animalType" value="<?= $dataInap['animal_type'] ? $dataInap['animal_type'] : '' ?>">
+                  </div>
+                </div>
+              </div>
+
+              <div class="row align-items-center">
+                <label for="race" class="col-auto col-form-label">Race :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="race" value="<?= $dataInap['race'] ? $dataInap['race'] : '' ?>">
+                  </div>
+                </div>
+              </div>
+              
+              <div class="row align-items-center">
+                <label for="color" class="col-auto col-form-label">Color :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="color" value="<?= $dataInap['color'] ? $dataInap['color'] : '--data kosong--' ?>">
+                  </div>
+                </div>
+              </div>
+
+              <div class="row align-items-center">
+                <label for="gender" class="col-auto col-form-label">Gender :</label>
+                <div class="col">
+                  <div class="underline-wrapper">
+                    <input type="text" readonly class="form-control-plaintext py-0 text-end" id="gender" value="<?= $dataInap['gender'] ? $dataInap['gender'] : '--data kosong--' ?>">
+                  </div>
+                </div>
+              </div>
+        </div>
+        <div class="column col-6">
+            <div class="row align-items-start">
+              <label for="diagnosa" class="col-auto col-form-label">Diagnosa :</label>
+              <div class="col d-flex justify-content-end">
+              <textarea class="form-control" name="diagnosis" id="input diagnosis" rows="3"><?= $dataInap['diagnosis'] ? $dataInap['diagnosis'] : '' ?></textarea>
               </div>
             </div>
-          </div>
 
-          <div class="col-md-3 mt-3">
-            <label for="inputFurColor" class="form-label text-truncate">Phone Number</label>
-            <div>
-              <input type="text" name="phoneNumber" id="inputPhone" class="form-control <?=($phoneNumber) ? 'is-invalid' : ''; ?>" id="inputPhoneNumber" placeholder="Type Here...."  value="<?= $dataInap['phone'] ? $dataInap['phone'] : '' ?>">
-              <div class="invalid-feedback">
-                <?= $phoneNumber ?>
+            <div class="row align-items-start my-2">
+              <label for="prognosa" class="col-auto col-form-label">Prognosa :</label>
+              <div class="col d-flex justify-content-end">
+              <textarea class="form-control" name="prognosa" id="inputPrognosa" rows="3"><?= $dataInap['prognosa'] ? $dataInap['diagnosis'] : '' ?></textarea>
               </div>
             </div>
-          </div>
 
-          <div class="col-md-3 mt-3">
-            <label for="inputAnimalType" class="form-label text-truncate">Animal Type</label>
-            <div>
-              <input type="text" name="animalType" class="form-control <?=($animalType) ? 'is-invalid' : ''; ?>" id="inputAnimalType" placeholder="Type Here...." value="<?= $dataInap['animal_type'] ? $dataInap['animal_type'] : '' ?>">
-              <div class="invalid-feedback">
-                <?= $animalType ?>
+            <div class="row align-items-start">
+              <label for="medication" class="col-4 col-form-label wrap-text">Rencana Pengobatan :</label>
+              <div class="col d-flex justify-content-end">
+              <textarea class="form-control" name="prognosa" id="inputMedication" rows="3"><?= $dataInap['medication'] ? $dataInap['medication'] : '' ?></textarea>
               </div>
             </div>
-          </div>
-
-          <div class="col-md-3 mt-3">
-            <label for="inputFurColor" class="form-label text-truncate">Race</label>
-            <div>
-              <input type="text" name="race" class="form-control <?=($race) ? 'is-invalid' : ''; ?>" id="inputRace" placeholder="Type Here...." value="<?= $dataInap['race'] ? $dataInap['race'] : '' ?>">
-              <div class="invalid-feedback">
-                <?= $race ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-3 mt-3">
-            <label for="inputFurColor" class="form-label text-truncate">Color</label>
-            <div>
-              <input type="text" name="color" class="form-control <?=($color) ? 'is-invalid' : ''; ?>" id="inputFurColor" placeholder="Type Here...." value="<?= $dataInap['color'] ? $dataInap['color'] : '' ?>">
-              <div class="invalid-feedback">
-                <?= $color ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6 mt-3">
-          <label for="inputGender" class="form-label text-truncate">Gender</label>
-          <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="radioMale" value="male"  <?= ($dataInap['gender'] === 'male') ? 'checked' : '' ?>>
-              <label class="form-check-label" for="radioMale">
-                  Male
-              </label>
-          </div>
-          <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="radioFemale" value="female"  <?= ($dataInap['gender'] === 'female') ? 'checked' : '' ?>>
-              <label class="form-check-label" for="radioFemale">
-                  Female
-              </label>
-          </div>
-          <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="radioUndefined" value="undefined" <?= ($dataInap['gender'] !== 'male' && $dataInap['gender'] !== 'female') ? 'checked' : '' ?>>
-              <label class="form-check-label" for="radioUndefined">
-                  Undefined
-              </label>
-          </div>
-      </div>
-
-          
-            <div class="col-md-6 align-items-start form-check form-switch my-4">
-              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckHospitalization">
-              <label class="form-check-label" for="flexSwitchCheckHospitalization">Tindak lanjut Rawat inap</label>
-            </div>
-
+        </div>
+  </div>
         
         <div class="container">
           <div class="row clearfix">
@@ -190,7 +170,8 @@
               </table>
           </div>
         </div>
-        <button id="add_row" class="btn btn-default pull-left">Add Row</button><button id='delete_row' class="pull-right btn btn-default">Delete Row</button>
+        <button id="add_row" class="btn btn-default pull-left">Add Row</button>
+        <button id='delete_row' class="pull-right btn btn-default">Delete Row</button>
    </div>
 
    <div class="d-flex justify-content-center">
--- app/Views/layout/_sidebar.php
+++ app/Views/layout/_sidebar.php
@@ -1,13 +1,12 @@
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="/">
-        <img src="<?= base_url('assets/img/bcpet_logo.png') ?>" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">BC Pet House</span>
-      </a>
+         <a class="navbar-brand m-0" href="/">
+          <img src="<?= base_url('assets/img/bcpet_logo.png') ?>" class="navbar-brand-img h-100 justify-content-center align-middle" alt="main_logo">
+          <span class="ms-1 font-weight-bold me-3 ">BC Pet House</span>
+        </a>
     </div>
     <hr class="horizontal dark mt-0">
-    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
       <ul class="navbar-nav" id="sidebar-menu">
 
         <li class="nav-item">
